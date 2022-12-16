31546bc89d3b72e8727f6c0ec085be1fbbf3193d
Bruno Ruotolo <brunoaruotolo@gmail.com>
2022-12-16 08:30:11 -0300

    Update MostPopular.jsx

--- src/components/home/MostPopular.jsx
+++ src/components/home/MostPopular.jsx
@@ -39,6 +39,7 @@ export default function MostPopular() {
           navigate("/");
         } else {
           __swalErrorMessage("Something got wrong", "Please, Try to login again!");
+          localStorage.removeItem("auth");
           navigate("/");
           setPageLoading(false);
         }
